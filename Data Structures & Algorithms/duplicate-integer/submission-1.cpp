a9bcbf29224c02520068219ca321fbe99db6a3e0
Daniel Chen <40429714+tingul4@users.noreply.github.com>
2026-07-31 15:47:10 +0800

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        vector<int> existNums;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < existNums.size(); j++) {
+                if (nums[i] == existNums[j]) return true; 
+            }
+            existNums.push_back(nums[i]);
+        }
+        return false;
+    }
+};
