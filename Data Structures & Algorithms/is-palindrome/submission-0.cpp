db86e25beac47f578b2aefe0b3bce504a1f10a06
Daniel Chen <40429714+tingul4@users.noreply.github.com>
2026-08-07 18:38:03 +0800

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length() - 1;
+        char cI, cJ;
+        while (i < j) {
+            // i turn
+            while ((s[i] < 'a' || s[i] > 'z') && (s[i] < 'A' || s[i] > 'Z') && (s[i] < '0' || s[i] > '9')) i++;
+            cI = tolower(s[i]);
+            // j turn
+            while ((s[j] < 'a' || s[j] > 'z') && (s[j] < 'A' || s[j] > 'Z') && (s[j] < '0' || s[j] > '9')) j--;
+            cJ = tolower(s[j]);
+            if (cI != cJ) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAlNum(char c) {
+        return (c > 'a' && c < 'z') ||
+            (c > 'A' && c < 'Z') ||
+            (c > '0' && c < '9');
+    }
+    bool isPalindrome(string s) {
+        int i = 0, j = static_cast<int>(s.length()) - 1;
+        while (i < j) {
+            while (i < j && !isAlNum(s[i])) i++;
+            while (i < j && !isAlNum(s[j])) j--;
+            if (tolower(s[i]) != tolower(s[j])) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
