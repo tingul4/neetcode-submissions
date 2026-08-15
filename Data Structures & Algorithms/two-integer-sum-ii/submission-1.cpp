73c1cab966809aa9949ed46fdcc88ae033996ab2
Daniel Chen <40429714+tingul4@users.noreply.github.com>
2026-08-16 00:06:25 +0800

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int max = numbers.size() - 1;
+        int i = 0;
+        vector<int> ans;
+        while (i != max) {
+            int j = i + 1;
+            while (j != max) {
+                if (numbers[i] + numbers[j] == target) return {i+1, j+1};
+                else if (numbers[i] + numbers[j] < target) j++;
+                else max = j;
+            }
+            i++;
+        }
+        return ans;
+    }
+};
